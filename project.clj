db20d7858f6942f4cd99ea7674de8c44412dcbf5
Jon Eisen <jon@joneisen.me>
2015-12-14 12:28:07 -0700

    Version 0.1.3-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject denvr "0.1.2"
+(defproject denvr "0.1.3-SNAPSHOT"
   :description "Development Environment Reimagined.
                A CLI manager for managing and sharing
                development environment configurations."
